3ddfe26b00712be985d9ef0d59535c707040e7cc
fitriyanianisa18 <fitriyanianisa18@gmail.com>
2025-08-06 22:07:59 +0700

    Menambahkan todo CRUD dan DB postgresql

---
+++ src/config/testDb.js
@@ -0,0 +1,15 @@
+const pool = require('./db')
+
+async function testConnection() {
+    try {
+        const res = await pool.query("SELECT NOW()")
+        console.log('Koneksi berhasil', res.rows[0].now)
+    }catch (error){
+        console.log('Koneksi gagal', error.message)
+        } finally {
+            await pool.end()
+        }
+    }
+
+
+testConnection()
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 
 // import router
 const router = require('./routes/router');
 const userRouter = require('./routes/userRouter')
-
+const todoRouter = require('./routes/todoRouter')
 
 // import custom middleware
-const {logger} = require('./middleware/logger')
+const {logger} = require('./middleware/logger');
 
+// inisiasi instance express dalam variabel app
 const app = express();
 
 // use middleware
 app.use(logger)
+app.use(compression())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -21,5 +24,6 @@ app.use(cors({ origin: true, credentials: true }));
 
 app.use('/', router);
 app.use('/api/user', userRouter)
+app.use('/api/todo', todoRouter)
 
 app.listen(process.env.SERVER_PORT, () => {console.log('Server Running')});
